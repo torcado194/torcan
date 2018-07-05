80a42650e43175c2d9eb4862ca0b2ed02596dd5e
torcado <torcado@yahoo.com>
2018-07-05 00:38:25 -0700

    custom aspect ratio, parent containers, cover and contain options, fix viewport values, add example

--- README.md
+++ README.md
@@ -3,26 +3,82 @@ another canvas library
 
 
 ## usage
-torcan makes a fullscreen canvas that scales with the device. It creates a viewport box that is always contained and centered within the screen.
+torcan is built to set up a fullscreen canvas element which remains visually consistent across all devices by scaling and positioning a viewport
 
-```
-var t = new torcan();
+```js
+let t = new torcan();
 t.init(document.getElementById("canvas")); //give torcan an existing canvas element
+```
 
-t.c //canvas context
+you can specify the viewport size and aspect ratio, as well as if the viewport should cover or be contained in the window
+
+```js
+let t = new torcan(160, 90, true); //this canvas has a virtual viewport of size 160x90, and covers the screen
+let t = new torcan(100, 100, true); //size 100x100 (default square), and is contained in the screen
+let t = new torcan(); //same as previous
+```
+
+it also handles both touch and mouse inputs, and tweens touch positions
+
+```js
 t.cursorHandler; //handles mouse and touch
 t.cursorHandler.x //x and y of touch/mouse
 t.cursorHandler.y
 
 t.press //overload, called when user clicks or taps
 ```
-   
-torcan will make your canvas visually identical on all devices
-
-it works by fullscreening the canvas and setting up a viewport box which scales with and is always contained in the screen
-
-it also handles both touch and mouse inputs, and tweens touch positions
 
 [here's an example project](http://torcado.com/toys/chromab.html)
 
 ![chromab gif](http://torcado.com/chromab.gif)
+
+
+------
+
+torcan now allows for a contained canvas. just specify the parent element in `init`
+
+```js
+let t = new torcan();
+t.init(document.getElementById("canvas"), document.getElementById("parent"));
+```
+
+this will cause the canvas to act as if the parent was the window, and will even resize on changes to the parent
+
+[here's an example utilizing aspect ratio and container elements](http://torcado.com/toys/torcantest.html)
+
+
+## properties
+
+#### canvas components
+
+```js
+t.canvas //canvas element
+t.c //canvas context
+```
+
+#### viewport
+```js
+t.scale //the virtual scale of the canvas. use this to convert from virtual to display pixels
+
+t.w //width and height of viewport, same as width/height passed into constructor
+t.h
+t.viewX //x and y of top left of visible canvas, relative to virtual (0,0) and based on virtual scale
+t.viewY
+t.viewW //width and height of visible canvas, based on virtual scale
+t.viewH
+t.viewLeft //left edge of visible canvas, identical to viewX
+t.viewRight //right edge of visible canvas
+t.viewTop //top edge of visible canvas, identical to viewY
+t.viewBottom //bottom edge of visible canvas
+```
+
+#### cursor handler
+```js
+t.cursorHandler; //handles mouse and touch
+t.cursorHandler.x //x and y of touch/mouse
+t.cursorHandler.y
+
+t.tweenTouch //decides if touches should tween the cursor position. default true, set to false to turn off
+
+t.press //overload with function, called when user clicks or taps
+```
--- torcan.js
+++ torcan.js
@@ -1,58 +1,56 @@
 (function(window){
     
-    window.torcan = function(){
-
-        var torcan = this;
-        var t = torcan;
-
+    window.torcan = function(w = 100, h = 100, cover = false){
+        
+        let torcan = this;
+        let t = torcan;
+        
         t.scale = 1;
         //TODO: allow user specified virtual size and aspect ratio
-        t.w = 100; //virtual width/height
-        t.h = 100;
+        t.w = w; //virtual width/height
+        t.h = h;
         t.offsetH = 0;
         t.offsetV = 0;
-
-        t.canvasX = document.getElementById("canvas").offsetLeft;
-        t.canvasY = document.getElementById("canvas").offsetTop;
+        t.tweenTouch = true;
         
         t.tweenQueue = [];
         
         t.Tween = function(prop, from, to, duration){
-
-            var tween = this;
-
+            
+            let tween = this;
+            
             this.prop = prop;
             this.from = from;
             this.to = to;
             this.duration = duration;
-
-            var exists = false;
-
-            for(var i = 0; i < t.tweenQueue.length; i++){
+            
+            let exists = false;
+            
+            for(let i = 0; i < t.tweenQueue.length; i++){
                 if(t.tweenQueue[i].prop === prop){
                     exists = true;
                     t.tweenQueue.splice(i, 1);
                 }
             }
-
-            var search = prop.split(".");
-            var item = search[search.length - 1];
-            var endpoint = torcan;
-            for(var i = 0; i < search.length - 1; i++){
+            
+            let search = prop.split(".");
+            let item = search[search.length - 1];
+            let endpoint = torcan;
+            for(let i = 0; i < search.length - 1; i++){
                 endpoint = endpoint[search[i]];
             }
             if(endpoint[item] === undefined){
                 return false;
             }
             endpoint[item] = from;
-            var start = Date.now();
+            let start = Date.now();
             this.step = function(endpoint){
-                var now = Date.now();
-                var t = (now - start) / duration;
-
+                let now = Date.now();
+                let t = (now - start) / duration;
+                
                 endpoint[item] = from + ((to - from) * t)
                 console.log(endpoint[item]);
-
+                
                 if(now - start > duration){
                     endpoint[item] = to;
                 } else {
@@ -61,11 +59,11 @@
             }
             this.step(endpoint);
         }
-
+        
         //manage mouse and touch control
-        var cursorHandler = function(){
-
-            var h = this; //handler
+        let cursorHandler = function(){
+            
+            let h = this; //handler
             h.x = 0; //final relative cursor position (within virtual size and offset)
             h.y = 0;
             //cursor is mouse/touch agnostic
@@ -76,7 +74,7 @@
             h.cursorStartY = 0;
             h.clickRange = 20; //how far from origin until press becomes drag (taxi distance)
             h.dragRange = 40;
-
+            
             h.pressed = false;
             h.dragging = false;
             h.usingTouch = false; //changes per action, if for some reason a user has both
@@ -84,7 +82,7 @@
             h.press = function(){
                 //template, overritten by user
             }
-
+            
             h.mouseDown = function(e){
                 h.pressed = true;
                 h.usingTouch = false;
@@ -95,7 +93,7 @@
                     h.cursorStartY = h.cursorY;
                 }
             }
-
+            
             h.touchDown = function(e){
                 h.pressed = true;
                 h.usingTouch = true;
@@ -106,17 +104,17 @@
                     h.cursorStartY = h.cursorY;
                 }
             }
-
+            
             h.mouseUp = function(e){
                 h.pressed = false;
                 h.cursorStart = false;
                 h.mouseXY(e);
-
+                
                 if((Math.abs(h.cursorX - h.cursorStartX) < h.clickRange) && (Math.abs(h.cursorY - h.cursorStartY) < h.clickRange)){
                     h.press();
                 }
             }
-
+            
             h.touchUp = function(e){
                 h.pressed = false;
                 h.cursorStart = false;
@@ -124,72 +122,97 @@
                     h.press();
                 }
             }
-
+            
             h.mouseXY = function(e){
                 if (!e)
-                    var e = window.event;
+                    e = window.event;
                 h.cursorX = e.pageX - t.canvasX;
                 h.cursorY = e.pageY - t.canvasY;
                 h.x = ((h.cursorX - (t.offsetH / 2)) / t.scale);
                 h.y = ((h.cursorY - (t.offsetV / 2)) / t.scale);
             }
-
+            
             h.touchXY = function(e){
                 if (!e)
-                    var e = window.event;
+                    e = window.event;
                 e.preventDefault();
                 if((Math.abs(h.cursorX - h.cursorStartX) < h.dragRange) && (Math.abs(h.cursorY - h.cursorStartY) < h.dragRange)){
                     h.dragging = false;
-
+                    
                     h.cursorX = e.targetTouches[0].pageX - t.canvasX;
                     h.cursorY = e.targetTouches[0].pageY - t.canvasY;
                     
-                    t.tweenQueue.push(new t.Tween("cursorHandler.x", h.x, (h.cursorX - (t.offsetH / 2)) / t.scale, 100));
-                    t.tweenQueue.push(new t.Tween("cursorHandler.y", h.y, (h.cursorY - (t.offsetV / 2)) / t.scale, 100));
+                    if(t.tweenTouch){
+                        t.tweenQueue.push(new t.Tween("cursorHandler.x", h.x, (h.cursorX - (t.offsetH / 2)) / t.scale, 100));
+                        t.tweenQueue.push(new t.Tween("cursorHandler.y", h.y, (h.cursorY - (t.offsetV / 2)) / t.scale, 100));
+                    } else {
+                        h.x = ((h.cursorX - (t.offsetH / 2)) / t.scale);
+                        h.y = ((h.cursorY - (t.offsetV / 2)) / t.scale);
+                    }
                 } else {
                     h.dragging = true;
                     h.cursorStartX = null;
                     h.cursorStartY = null;
-
+                    
                     h.cursorX = e.targetTouches[0].pageX - t.canvasX;
                     h.cursorY = e.targetTouches[0].pageY - t.canvasY;
-
+                    
                     h.x = ((h.cursorX - (t.offsetH / 2)) / t.scale);
                     h.y = ((h.cursorY - (t.offsetV / 2)) / t.scale);
                 }
             }
         };
         
-        t.setup = function(){
-            t.canvas.width = window.innerWidth;
-            t.canvas.height = window.innerHeight;
+        t.setup = function(mutation){
+            if(Array.isArray(mutation)){
+                t.canvas.width  = getWidth(t.parent, mutation);
+                t.canvas.height = getHeight(t.parent, mutation);
+            } else {
+                t.canvas.width  = t.parent ? getWidth(t.parent)  : window.innerWidth;
+                t.canvas.height = t.parent ? getHeight(t.parent) : window.innerHeight;
+            }
+            
             t.offsetH = 0;
             t.offsetV = 0;
-            var width = document.getElementById("canvas").width;
-            var height = document.getElementById("canvas").height;
-            if(width < height){ //set viewport size to largest square
-                t.offsetV = t.canvas.height - t.canvas.width;
-                t.scale = (width / t.w);
-            } else {
-                t.offsetH = t.canvas.width - t.canvas.height;
-                t.scale = (height / t.h);
+            
+            if(cover){ //set viewport size to smallest covering square
+                if((t.canvas.width / t.canvas.height) < (t.w / t.h)){
+                    t.offsetH = ((t.canvas.width / t.canvas.height) - (t.w / t.h)) * t.canvas.height;
+                    t.scale = (t.canvas.height / t.h);
+                } else {
+                    t.offsetV = ((t.canvas.height / t.canvas.width) - (t.h / t.w)) * t.canvas.width;
+                    t.scale = (t.canvas.width / t.w);
+                }
+            } else { //set viewport size to largest contained square
+                if((t.canvas.width / t.canvas.height) < (t.w / t.h)){
+                    t.offsetV = ((t.canvas.height / t.canvas.width) - (t.h / t.w)) * t.canvas.width;
+                    t.scale = (t.canvas.width / t.w);
+                } else {
+                    t.offsetH = ((t.canvas.width / t.canvas.height) - (t.w / t.h)) * t.canvas.height;
+                    t.scale = (t.canvas.height / t.h);
+                }
             }
+            
             t.c.setTransform(t.scale, 0, 0, t.scale, t.offsetH / 2, t.offsetV / 2); //center and scale viewport
-            t.viewX = (t.offsetH / 2) / t.scale;
-            t.viewY = (t.offsetV / 2) / t.scale;
+            t.viewX = -(t.offsetH / 2) / t.scale;
+            t.viewY = -(t.offsetV / 2) / t.scale;
             t.viewW = t.w + (t.offsetH / t.scale);
             t.viewH = t.h + (t.offsetV / t.scale);
-            t.viewRight  = t.viewW - t.viewX;
-            t.viewLeft   = 0 - t.viewX;
-            t.viewBottom = t.viewH - t.viewY;
-            t.viewTop    = 0 - t.viewY;
+            t.viewRight  = t.viewX + t.viewW;
+            t.viewLeft   = t.viewX;
+            t.viewBottom = t.viewY + t.viewH;
+            t.viewTop    = t.viewY;
         }
         
-        t.init = function(canvas){
+        t.init = function(canvas, parent){
             t.canvas = canvas;
+            t.parent = parent;
             t.c = t.canvas.getContext('2d');
             t.c.imageSmoothingEnabled = true;
             t.c.globalCompositeOperation = 'source-over';
+            
+            t.canvasX = canvas.offsetLeft;
+            t.canvasY = canvas.offsetTop;
             
             t.cursorHandler = new cursorHandler();
             
@@ -204,7 +227,31 @@
             document.body.addEventListener("touchcancel", t.cursorHandler.touchUp, false);
             
             t.setup();
-            window.addEventListener("resize", t.setup, false);
+            
+            if(parent){
+                let observer = new MutationObserver(t.setup);
+                observer.observe(parent, { attributes: true });
+            } else {
+                window.addEventListener("resize", t.setup, false);
+            }
+        }
+    }
+    
+    function getWidth(element, mutation){
+        let style = window.getComputedStyle(element, null);
+        if(mutation && style.boxSizing !== "border-box"){
+            return parseFloat(mutation[0].target.style.width);
+        } else {
+            return style.boxSizing === "border-box" ? parseFloat(style.width) - (parseFloat(style.paddingLeft) + parseFloat(style.paddingRight)) - (parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth)) : parseFloat(style.width);
+        }
+    }
+    
+    function getHeight(element, mutation){
+        let style = window.getComputedStyle(element, null);
+        if(mutation && style.boxSizing !== "border-box"){
+            return parseFloat(mutation[0].target.style.height);
+        } else {
+            return style.boxSizing === "border-box" ? parseFloat(style.height) - (parseFloat(style.paddingTop) + parseFloat(style.paddingBottom)) - (parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth)) : parseFloat(style.height);
         }
     }
     
